194abf914ca638467774055837c8d5ec6d267a1c
Erick-Toala <94879384+Erick-Toala@users.noreply.github.com>
2022-12-27 19:51:29 -0500

    Add files via upload

---
+++ src/app/components/usuarios/usuarios.component.ts
@@ -0,0 +1,33 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { UsuariosService } from '../../servicios/usuarios.service';
+import { Usuarios } from '../../models/usuarios';
+@Component({
+  selector: 'app-usuarios',
+  templateUrl: './usuarios.component.html',
+  styleUrls: ['./usuarios.component.css']
+})
+export class UsuariosComponent {
+
+  //array para los usuarios que serán almacenados en el local storage
+  @Input() usuarios: Usuarios[] = [];
+
+  constructor(
+    //private _CargaScripts:CargarScriptsService,
+    public usuariosService: UsuariosService,
+    )
+  {
+    //_CargaScripts.Carga(["main"]);
+  }
+  ngOnInit(){
+    //se obtiene los usuarios
+    this.usuarios = this.usuariosService.getUsuarios();
+  }
+  deleteUsuarios(usuarios: Usuarios){
+    for(let i=0; i< this.usuarios.length; i++){
+      if (usuarios == this.usuarios[i]){
+        this.usuarios.splice(i, 1);
+        localStorage.setItem('usuarios',JSON.stringify(this.usuarios));
+      }
+    }
+  }
+}
